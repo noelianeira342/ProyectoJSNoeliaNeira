8e3ce113908b775c6a449037763dbbf9c02e1594
noelianeira342 <90524497+noelianeira342@users.noreply.github.com>
2021-12-02 19:35:34 -0300

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -22,6 +22,50 @@ function borrarElemento(){
     lista.removeChild(lista.lastChild);
 
 }
+let botonesCompra = document.querySelectorAll(".botonCompra");
+let carrito = [];
+
+
+
+for( let boton of botonesCompra){
+
+    boton.addEventListener("click" , agregarCarrito);
+
+}
+
+
+console.log(botonesCompra);
+
+
+
+function agregarCarrito(e){
+
+    console.log(e.target);
+    let hijo = e.target;
+    let padre = hijo.parentNode.parentNode;
+    let titulo = padre.querySelector("h5").textContent;
+    let parrafo = padre.querySelector("p").textContent;
+    let imagen = padre.querySelector("img").src;
+
+
+    let producto = {
+        nombre: titulo,
+        desc:parrafo,
+        img: imagen
+    };
+
+
+    carrito.push(producto)
+    console.log(padre);
+    console.log(titulo);
+
+}
+
+
+
+
+
+
 
 
 
